65b57cb874cda91122cba472e260463a8293701f
silingyuan <1062384716@qq.com>
2015-11-21 15:14:38 +0800

    tianjian

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,6 +2,7 @@
 namespace Home\Controller;
 use Think\Controller;
 use Org\Net\Http;
+use Think\Verify;
 class IndexController extends Controller {
     public function index(){
     	echo "sdf";
@@ -18,4 +19,15 @@ class IndexController extends Controller {
      public function testDownload(){
      	Http::download("Image/ggk.png","图片");
      }
+     /**
+      * 测试功能：测试http类的下载功能
+      */
+     public function testVerify(){
+     	$verify=new Verify();
+     	$verify->entry();
+     }
+     public function test(){
+     	$this->display();
+     }
+     
 }
